8e942c786817d256ce2ce072461666e224be22ce
ggc1412 <ggc1412@gmail.com>
2020-05-05 23:24:57 +0900

    :file_folder: Form props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
 import Form from './components/Form';
 import "./App.css";
 
-const App = () => {
-  return (
-    <div>
-      <TodoListTemplate form={<Form/>}>
-        todo list
-      </ TodoListTemplate>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    input:'',
+    todos:[]
+  }
+
+  onCreate = () => {
+    const { input, todos } = this.state;
+    this.setState({
+      input:'',
+      todos: [input, ...todos]
+    })
+  }
+
+  onChange = (e) => {
+    const { value } = e.target;
+    this.setState({
+      input: value
+    })
+  }
+
+  onKeyPress = (e) => {
+    if(e.key === "Enter"){
+      this.onCreate();
+    }
+  }
+
+  render(){
+    const { input } = this.state;
+    return (
+      <div>
+        <TodoListTemplate form={<Form
+          value={input}
+          onChange={this.onChange}
+          onKeyPress={this.onKeyPress}
+          onCreate={this.onCreate}
+        />}>
+          todo list
+        </ TodoListTemplate>
+      </div>
+    );
+  }
 }
 
 export default App;
