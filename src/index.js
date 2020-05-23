ae78b4bbc4f34b90f7598d54dcdcbb37c76ec84c
ggc1412 <ggc1412@gmail.com>
2020-05-23 23:50:59 +0900

    :hammer:store생성 분리

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
-import { createStore } from "redux";
-import reducer from "./modules";
 import { Provider } from "react-redux";
-// redux store 생성, provider 연결
-// 스토어에 리듀서 연결
-
-const store = createStore(reducer);
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
